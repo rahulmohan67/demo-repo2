ec039b1e6c0e5c51b8881145dce925772fecf841
Rahul Mohan <mohan2404.rahul@gmail.com>
2020-07-31 18:03:01 +0530

    Added the source code

---
+++ coins.py
@@ -0,0 +1,14 @@
+denom=[1,2,5,10,20,50,100,200,500]
+denom.reverse()
+amt=117
+out=[]
+n=len(denom)
+i=0
+while(i<n):
+    while(amt>=denom[i]):
+        amt-=denom[i]
+        out.append(denom[i])
+    i+=1
+for i in range(len(out)):
+    print(out[i])
+
